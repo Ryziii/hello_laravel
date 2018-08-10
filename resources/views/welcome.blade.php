301b55c1beab53ca1f8d24bbd76e5b437fd1ec02
Ryziii <vagrant@homestead>
2018-08-10 14:56:48 +0000

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hi！Laravel--by Ryziii
+                    I Love You 劳颖榆！
                 </div>
 
                 <div class="links">
